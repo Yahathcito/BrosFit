f94bb86509e9824d59a01df2b2c62ef095545cc9
grojas23 <gahel.rojas.villalobos@est.una.ac.cr>
2025-09-10 10:27:21 -0600

    prueba 2

--- BrosFitt/Instructor.h
+++ BrosFitt/Instructor.h
@@ -1,5 +1,34 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
 class Instructor
 {
+private:
+	string cedula;
+	string nombre;
+	int telefono;
+	string fechaNacimiento;
+	string correo;
+
+public:
+
+	Instructor();
+	Instructor(string cedula, string nombre, int telefono, string fechaNacimiento, string correo);
+	string getCedula();
+	void setCedula(string cedula);
+	string getNombre();
+	void setNombre(string nombre);
+	int getTelefono();
+	void setTelefono(int telefono);
+	string getFechaNacimiento();
+	void setFechaNacimiento(string fechaNacimiento);
+	string getCorreo();
+	void setCorreo(string correo);
+	string toString();
+	~Instructor();
+
 };
 
---
+++ BrosFitt/coleccionInstructores.cpp
@@ -0,0 +1,62 @@
+#include "coleccionInstructores.h"
+
+
+ColeccionInstructores::ColeccionInstructores(int tam) {
+	this->tam = tam;
+	cantidad = 0;
+	instructores = new Instructor * [tam];
+	for (int i = 0; i < tam; i++) {
+		instructores[i] = nullptr;
+	}
+}
+ColeccionInstructores::~ColeccionInstructores() {
+	for (int i = 0; i < cantidad; i++) {
+		delete instructores[i];
+	}
+	delete[] instructores;
+}
+void ColeccionInstructores::agregarInstructor(Instructor* instructor) {
+	if (cantidad < tam) {
+		instructores[cantidad] = instructor;
+		cantidad++;
+	}
+	else {
+		cout << "No se puede agregar más instructores, capacidad máxima alcanzada." << endl;
+	}
+}
+Instructor* ColeccionInstructores::buscarInstructor(string cedula) {
+	for (int i = 0; i < cantidad; i++) {
+		if (instructores[i]->getCedula() == cedula) {
+			return instructores[i];
+		}
+	}
+	return nullptr;
+}
+bool ColeccionInstructores::eliminarInstructor(string cedula) {
+	for (int i = 0; i < cantidad; i++) {
+		if (instructores[i]->getCedula() == cedula) {
+			delete instructores[i];
+			for (int j = i; j < cantidad - 1; j++) {
+				instructores[j] = instructores[j + 1];
+			}
+			instructores[cantidad - 1] = nullptr;
+			cantidad--;
+			return true;
+		}
+	}
+	return false;
+}
+string ColeccionInstructores::listarInstructores() {
+	string resultado = "";
+	for (int i = 0; i < cantidad; i++) {
+		resultado += instructores[i]->toString() + "\n";
+	}
+	return resultado;
+}
+int ColeccionInstructores::getCantidad() {
+	return cantidad;
+}
+
+int ColeccionInstructores::getTam() {
+	return tam;
+}
